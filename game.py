88978501d92df3b4a01580d5129b6f85b4d317e1
kneckebrodet <113566464+kneckebrodet@users.noreply.github.com>
2023-04-29 15:46:45 +0900

    Update game.py

--- game.py
+++ game.py
@@ -3,37 +3,40 @@ from interface import NewGameWindow, CountdownWindow, RankingWindow
 from mqtt import MQTTClient
 import threading
 
-## SET AND CONNECT TO DATABASE
+## Set and connect to database
 mongo = MongoDB()
 mongo.connect_to_localhost()
 
-## SET AND CONNECT TO MQTT
+## SET and connect to MQTT
 mqtt_client = MQTTClient("broker_address", 1883, "username", "password")
 mqtt_client.connect()
 
 game_status = 1
 while game_status:
-    ## OPEN NEW GAME WINDOW AND GET PLAYER NAME, SEND -START- SIGNAL TO BROKER
+    ## Open a new-game window and get the player name
     new_game_window = NewGameWindow()
     new_player_name = new_game_window.run()
-    mqtt_client.publish("test", 1)
+    # Send start signal to broker
+    mqtt_client.publish("topic", 1)
 
+    # Begin countdown and start the game.
     count_down_window = CountdownWindow()
-    target = count_down_window.run()
-    target=mqtt_client.subscribe("test")
+    count_down_window.run()
+    
+    # Get the scores from broker
+    mqtt_client.subscribe("topic")
+    new_player_points = mqtt_client.get_points()
 
-    #mqtt_thread.join()
-    new_player_points =  mqtt_client.get_points()
-
-    ## RECIEVE EARNED POINTS FROM BROKER AND ADD TO DATABASE
-    print(new_player_points)
+    # Create the player records and store in database
     player_data = {new_player_name: new_player_points}
     mongo.add_new_player(player_data)
 
-    ## OPEN RANKED LIST WINDOW, GET LIST FROM DATABASE AND DISPLAY TOP PLAYERS
+    # Retrieving all player records from database and display the top 9 players
     list_of_players = mongo.get_list_of_players()
     count_down_window.close_window()
     ranking_window = RankingWindow(list_of_players)
+    # if New Game-button is pressed: Go from the top again
+    # if Exit-button is pressed: Exit the game
     game_status = ranking_window.run()
 
 
