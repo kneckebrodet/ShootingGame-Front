a85bb35fd065b77fd0f9a3575fd7e9ff9e351c00
kneckebrodet <113566464+kneckebrodet@users.noreply.github.com>
2023-04-29 09:05:06 +0900

    Update game.py

--- game.py
+++ game.py
@@ -1,13 +1,14 @@
 from database import MongoDBClient
 from interface import NewGameWindow, CountdownWindow, RankingWindow
 from mqtt import MQTTClient
+import subprocess
 
 ## SET AND CONNECT TO DATABASE
 mongo = MongoDBClient()
 mongo.connect_to_localhost()
 
 ## SET AND CONNECT TO MQTT
-mqtt_client = MQTTClient("localhost", 1883, "admin", "123")
+mqtt_client = MQTTClient("172.19.8.186", 1883, "lucas", "rohlin")
 mqtt_client.connect()
 
 ## OPEN NEW GAME WINDOW AND GET PLAYER NAME, SEND -START- SIGNAL TO BROKER
@@ -18,10 +19,11 @@ mqtt_client.publish("test", 1)
 ## OPEN THE COUNTDOWN WINDOW WHILE GAME IS RUNNING
 count_down_window = CountdownWindow()
 # choose timelimit of game
-count_down_window.run(15)
+count_down_window.run()
+new_player_points =  38#mqtt_client.subscribe("test")
 
 ## RECIEVE EARNED POINTS FROM BROKER AND ADD TO DATABASE
-new_player_points = mqtt_client.subscribe("test")
+#new_player_points = mqtt_client.subscribe("test")
 print(new_player_points)
 player_data = {new_player_name: new_player_points}
 mongo.add_new_player(player_data)
