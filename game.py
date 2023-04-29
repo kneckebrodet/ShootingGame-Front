c3cb9b8b810b0a0e8e4d5ab477b8f28c1cfb95e1
kneckebrodet <113566464+kneckebrodet@users.noreply.github.com>
2023-04-29 10:57:49 +0900

    Update game.py

--- game.py
+++ game.py
@@ -1,38 +1,39 @@
-from database import MongoDBClient
+from mongodb import MongoDB
 from interface import NewGameWindow, CountdownWindow, RankingWindow
 from mqtt import MQTTClient
-import subprocess
+import threading
 
 ## SET AND CONNECT TO DATABASE
-mongo = MongoDBClient()
+mongo = MongoDB()
 mongo.connect_to_localhost()
 
 ## SET AND CONNECT TO MQTT
-mqtt_client = MQTTClient("172.19.8.186", 1883, "lucas", "rohlin")
+mqtt_client = MQTTClient("broker_address", 1883, "username", "password")
 mqtt_client.connect()
 
-## OPEN NEW GAME WINDOW AND GET PLAYER NAME, SEND -START- SIGNAL TO BROKER
-new_game_window = NewGameWindow()
-new_player_name = new_game_window.run()
-mqtt_client.publish("test", 1)
+game_status = 1
+while game_status:
+    ## OPEN NEW GAME WINDOW AND GET PLAYER NAME, SEND -START- SIGNAL TO BROKER
+    new_game_window = NewGameWindow()
+    new_player_name = new_game_window.run()
+    mqtt_client.publish("test", 1)
 
-## OPEN THE COUNTDOWN WINDOW WHILE GAME IS RUNNING
-count_down_window = CountdownWindow()
-# choose timelimit of game
-count_down_window.run()
-new_player_points =  38#mqtt_client.subscribe("test")
+    count_down_window = CountdownWindow()
+    target = count_down_window.run()
+    target=mqtt_client.subscribe("test")
 
-## RECIEVE EARNED POINTS FROM BROKER AND ADD TO DATABASE
-#new_player_points = mqtt_client.subscribe("test")
-print(new_player_points)
-player_data = {new_player_name: new_player_points}
-mongo.add_new_player(player_data)
+    #mqtt_thread.join()
+    new_player_points =  mqtt_client.get_points()
 
-## OPEN RANKED LIST WINDOW, GET LIST FROM DATABASE AND DISPLAY TOP PLAYERS
-list_of_players = mongo.get_list_of_players()
-ranking_window = RankingWindow(list_of_players)
-ranking_window.run()
+    ## RECIEVE EARNED POINTS FROM BROKER AND ADD TO DATABASE
+    print(new_player_points)
+    player_data = {new_player_name: new_player_points}
+    mongo.add_new_player(player_data)
 
-
+    ## OPEN RANKED LIST WINDOW, GET LIST FROM DATABASE AND DISPLAY TOP PLAYERS
+    list_of_players = mongo.get_list_of_players()
+    count_down_window.close_window()
+    ranking_window = RankingWindow(list_of_players)
+    game_status = ranking_window.run()
 
 
